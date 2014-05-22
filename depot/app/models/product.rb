35d795cf6d865a4a7379d4ee4b4168c1e56e6bdd
Sergio G <sergiorodriguezgijon@gmail.com>
2014-05-22 23:31:38 +0200

    Chapter 9. Task D: Cart Creation.

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,16 +1,25 @@
 class Product < ActiveRecord::Base
 
-	validates :title, :description, :image_url, presence: true
-	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	validates :title, uniqueness: true
-	validates :image_url, allow_blank: true, format: {
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
 		with: 	%r{\.(gif|jpg|png)\Z}i,
 		message:	'must be a URL for GIF, JPG or PNG image.'
 	}
+  has_many :line_items
+  before_destroy  :ensure_not_referenced_by_any_line_item
 
-	def self.latest
-		Product.order(:updated_at).last
-	end
+  def self.latest
+	Product.order(:updated_at).last
+  end
 
+  def ensure_not_referenced_by_any_line_item
+  	if line_items.empty?
+  		return true
+  	else
+  		errors.add(:base, "The product #{title} is referenced by #{line_items.size} line(s).")
+  	end
+  end
 
 end
