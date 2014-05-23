a66a4f26417d8e71efd01bfaab2b1efbd5813aff
Sergio G <sergiorodriguezgijon@gmail.com>
2014-05-24 01:03:13 +0200

    Chapter 10. Task E: A Smarter Cart.

--- depot/app/helpers/application_helper.rb
+++ depot/app/helpers/application_helper.rb
@@ -1,2 +1,41 @@
 module ApplicationHelper
+
+  def notice_color(notice)
+    if notice
+      type = type_notice(notice)
+      message = message_notice(type, notice)
+  		div_tag_head = "<div class=\"alert alert-#{type} alert-dismissable\">"
+  		cross_button = "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button>"
+  		notice_tag = div_tag_head + cross_button + message + "</div>"
+  		notice_tag.to_s.html_safe
+  	end # If's end.
+  end   # Def's end.
+
+  def type_notice(notice)
+    downcased = notice.downcase
+    if downcased.include? 'error' or downcased.include? 'invalid'
+      return'danger'
+    elsif downcased.include? 'done' or downcased.include? 'success'
+      return 'success'
+    elsif downcased.include? 'hey'
+      return 'warning'
+    else 
+      return 'info'
+    end 
+  end 
+
+  def message_notice(type, notice)
+    messages = {
+      :danger => 'Oops, something wrong happened',
+      :success => 'Done',
+      :warning => 'Hey, be careful',
+      :info => 'News, news'
+    }
+
+    "<strong>#{messages[type.to_sym]}!</strong> #{notice}"
 end
+
+
+
+private :type_notice, :message_notice
+end 
--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522214211) do
+ActiveRecord::Schema.define(version: 20140523125653) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
